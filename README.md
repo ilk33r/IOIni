f26f43f40c2058dd71836ca42af5aec4bf391763
ilker özcan <lkr_051@hotmail.com>
2016-09-14 14:55:03 +0300

    [skip ci] travis update.

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # IOIni
 ini file parser written with Swift
 
+|| **Build** |
+|---|---|
+|**macOS Xcode 7.3**       |[![Build Status](https://travis-ci.org/ilk33r/IOIni.svg?branch=master)](https://travis-ci.org/ilk33r/IOIni)|
+|**macOS Xcode 8**         |[![Build Status](https://travis-ci.org/ilk33r/IOIni.svg?branch=master)](https://travis-ci.org/ilk33r/IOIni)|
+|**Ubuntu 14.04**          |[![Build Status](https://travis-ci.org/ilk33r/IOIni.svg?branch=master)](https://travis-ci.org/ilk33r/IOIni)|
+|**Ubuntu 15.10**          |[![Build Status](https://travis-ci.org/ilk33r/IOIni.svg?branch=master)](https://travis-ci.org/ilk33r/IOIni)|
+
 ## Compile
 
 
--- Travis/DownloadDocker.sh
+++ Travis/DownloadDocker.sh
@@ -1,39 +1,20 @@
 #!/bin/bash
 
-#  run_docker.sh
-#  IORunner
+#  DownloadDocker.sh
+#  IOIni
 #
 #  Created by ilker özcan on 26/08/16.
 #
 
-if [[ $TEST_BRANCH == $TRAVIS_BRANCH ]]; then
 
-	if [[ ! -d docker ]]; then
-		mkdir docker
-	fi
+if [[ ! -d docker ]]; then
+	mkdir docker
+fi
 
-	if [[ -f docker/iorunner_${DOCKER_IMAGE}.tar ]]; then
-		docker import docker/iorunner_${DOCKER_IMAGE}.tar ilk3r/iorunner:${DOCKER_IMAGE}
-	else
-		docker pull ilk3r/iorunner:${DOCKER_IMAGE}
-		docker run --name iorunner_${DOCKER_IMAGE} ilk3r/iorunner:${DOCKER_IMAGE} /bin/true
-		docker export -o docker/iorunner_${DOCKER_IMAGE}.tar iorunner_${DOCKER_IMAGE}
-	fi
-
-elif [[ $TRAVIS_BRANCH == 'master' ]]; then
-
-	if [[ ! -d docker ]]; then
-		mkdir docker
-	fi
-
-	if [[ -f docker/iorunner_${DOCKER_IMAGE}.tar ]]; then
-		docker import docker/iorunner_${DOCKER_IMAGE}.tar ilk3r/iorunner:${DOCKER_IMAGE}
-	else
-		docker pull ilk3r/iorunner:${DOCKER_IMAGE}
-		docker run --name iorunner_${DOCKER_IMAGE} ilk3r/iorunner:${DOCKER_IMAGE} /bin/true
-		docker export -o docker/iorunner_${DOCKER_IMAGE}.tar iorunner_${DOCKER_IMAGE}
-	fi
-
+if [[ -f docker/iorunner_${DOCKER_IMAGE}.tar ]]; then
+	docker import docker/iorunner_${DOCKER_IMAGE}.tar ilk3r/iorunner:${DOCKER_IMAGE}
 else
-	exit 0
+	docker pull ilk3r/iorunner:${DOCKER_IMAGE}
+	docker run --name iorunner_${DOCKER_IMAGE} ilk3r/iorunner:${DOCKER_IMAGE} /bin/true
+	docker export -o docker/iorunner_${DOCKER_IMAGE}.tar iorunner_${DOCKER_IMAGE}
 fi
